cbd88cb367ef1c6609bcf74d2d52b00864911c46
Bizet Rodriguez <brodriguez-velez@ucmerced.edu>
2019-03-23 12:06:07 -0700

    Added MVC and pods

---
+++ Retreat/Retreat/View/ChatUINavigationViewController+Style.swift
@@ -0,0 +1,30 @@
+//
+//  ChatUINavigationViewController+Style.swift
+//  Retreat
+//
+//  Created by Bizet Rodriguez on 3/23/19.
+//  Copyright © 2019 Enrique Florencio. All rights reserved.
+//
+
+import UIKit
+
+class ChatUINavigationViewController_Style: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
